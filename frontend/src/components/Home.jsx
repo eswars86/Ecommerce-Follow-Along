84008d2cb2b97731ed7e59ab5906d24af528313a
unknown <lakshmimadhavi37@gmail.com>
2025-02-27 15:07:13 +0530

    Milestone-11

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,24 @@
 import Product from "./auth/Product";
 import Navbar from "./Navbar";
+
 const Home = () => {
-  // Example list of products
-  return(
-    <>
-    <Navbar/>
-    <Product />
-    </>
-  )
-}
+  return (
+    <div className="bg-gray-800 min-h-screen">
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Main Content */}
+      <div className="py-8 px-6 sm:px-12">
+        <h2 className="text-center text-4xl font-extrabold text-white mb-8">
+          Welcome to ChorBazar..
+        </h2>
+        {/* Product Section */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-1 gap-8">
+          <Product />
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Home;
