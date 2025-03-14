044040d7068ee4f9d1c4e1850cdaaa720340a30a
unknown <lakshmimadhavi37@gmail.com>
2025-03-14 11:53:00 +0530

    added files

--- frontend/src/components/auth/Showcase.jsx
+++ frontend/src/components/auth/Showcase.jsx
@@ -2,7 +2,7 @@ function Showcase() {
   return (
     <div className="bg-gray-700 flex justify-center items-center py-6">
       <img
-        src="https://i.pinimg.com/736x/dc/b4/b2/dcb4b2b8645578507926fc523abce4b1.jpg"
+        src="https://relay-bl-in-records.sgp1.cdn.digitaloceanspaces.com/GameNation/AdAsset_101"
         alt="Showcase"
         className="w-[95%] max-w-4xl rounded-lg shadow-lg transition-transform duration-300 hover:scale-105"
       />
